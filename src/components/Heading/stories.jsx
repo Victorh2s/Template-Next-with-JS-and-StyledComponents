bd5f1b2eacb57ce36f728902edde10c58a28520f
Victor Henrique <92759987+Victorh2s@users.noreply.github.com>
2022-06-07 14:01:50 -0300

    Template Next With Js and Styled Components

---
+++ src/components/Heading/stories.jsx
@@ -0,0 +1,14 @@
+import { Heading } from '.';
+
+export default {
+  title: 'Heading',
+  component: Heading,
+  args: {
+    children: 'O texto está escuro',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => <Heading {...args} />;
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,8 @@
+// import P from 'prop-types';
+import { Heading } from '../../components/Heading/index';
+
+function Home() {
+  return <Heading>Projeto Iniciado</Heading>;
+}
+
+export default Home;
